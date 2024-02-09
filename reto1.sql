74a8457833af5ce48a3e4041291e264ec3d360c5
Alexander <bryano_98@hotmail.com>
2024-02-08 19:56:09 -0500

    'done'

--- reto1.sql
+++ reto1.sql
@@ -1,12 +1,13 @@
--- Creacion de tablas
--- tabla Gerente
+--Creacion de tablas
+
+-- crea tabla Gerente
 CREATE TABLE Gerente (
     idGerente NUMBER PRIMARY KEY,
     descGerente VARCHAR2(100),
     fechaRegistro DATE
 );
 
--- tabla Condicion
+-- crea tabla Condicion
 CREATE TABLE Condicion (
     idCondicion NUMBER PRIMARY KEY,
     descCondicion VARCHAR2(100),
@@ -14,14 +15,14 @@ CREATE TABLE Condicion (
 );
 
 
--- tabla Provincia
+-- crea tabla Provincia
 CREATE TABLE Provincia (
     idProvincia NUMBER PRIMARY KEY,
     descProvincia VARCHAR2(100),
     fechaRegistro DATE
 );
 
--- tabla Distrito
+-- crea tabla Distrito
 CREATE TABLE Distrito (
     idDistrito NUMBER PRIMARY KEY,
     idProvincia NUMBER,
@@ -30,7 +31,7 @@ CREATE TABLE Distrito (
     FOREIGN KEY (idProvincia) REFERENCES Provincia(idProvincia)
 );
 
--- tabla Sede
+-- crea tabla Sede
 CREATE TABLE Sede (
     idSede NUMBER PRIMARY KEY,
     descSede VARCHAR2(100),
@@ -56,7 +57,7 @@ CREATE TABLE Hospital (
 );
 
 -- Inserción de registros en las tablas:
---Gerente
+-- Gerente
 INSERT INTO Gerente (idGerente, descGerente, fechaRegistro) VALUES (1, 'gerente 1', SYSDATE);
 INSERT INTO Gerente (idGerente, descGerente, fechaRegistro) VALUES (2, 'gerente 2', SYSDATE);
 INSERT INTO Gerente (idGerente, descGerente, fechaRegistro) VALUES (3, 'gerente 3', SYSDATE);
@@ -94,29 +95,43 @@ INSERT INTO Provincia (idProvincia, descProvincia, fechaRegistro) VALUES (5, 'pr
 
 /*
   Nombre del procedimiento: SP_HOSPITAL_REGISTRAR
-  Descripcion: Este procedimiento registra un nuevo hospital en la tabla Hospital
+  Descripción: Este procedimiento registra un nuevo hospital en la tabla Hospital
   Autor: Alexander
   Fecha de creación: 02-07-2024
   Versión: 1
 */
 
 
--- SP_HOSPITAL_REGISTRAR
+
 CREATE OR REPLACE PROCEDURE SP_HOSPITAL_REGISTRAR (
-    p_idHospital IN NUMBER,
-    p_idDistrito IN NUMBER,
+    p_idHospital IN VARCHAR2,
+    p_idDistrito IN VARCHAR2,
     p_nombre IN VARCHAR2,
-    p_antiguedad IN NUMBER,
-    p_area IN NUMBER,
-    p_idSede IN NUMBER,
-    p_idGerente IN NUMBER,
-    p_idCondicion IN NUMBER
+    p_antiguedad IN VARCHAR2,
+    p_area IN VARCHAR2,
+    p_idSede IN VARCHAR2,
+    p_idGerente IN VARCHAR2,
+    p_idCondicion IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO Hospital (idHospital, idDistrito, nombre, antiguedad, area, idSede, idGerente, idCondicion, fechaRegistro)
-    VALUES (p_idHospital, p_idDistrito, p_nombre, p_antiguedad, p_area, p_idSede, p_idGerente, p_idCondicion, SYSDATE);
-    COMMIT;
+    -- intenta insertar los valores en la tabla Hospital
+    BEGIN
+     INSERT INTO Hospital (idHospital, idDistrito, nombre, antiguedad, area, idSede, idGerente, idCondicion, fechaRegistro)
+        VALUES (p_idHospital, p_idDistrito, p_nombre, p_antiguedad, p_area, p_idSede, p_idGerente, p_idCondicion, SYSDATE);
+
+        -- muestra un mensaje de registro exitoso
+        DBMS_OUTPUT.PUT_LINE('Registro exitoso.');
+
+        -- confirma la transaccion
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            -- captura las expeciones
+            DBMS_OUTPUT.PUT_LINE('Error al registrar el hospital: ' || SQLERRM);
+            ROLLBACK;
+    END;
 END SP_HOSPITAL_REGISTRAR;
+
 
 
 -- ejecucion del procedimiento almacenado SP_HOSPITAL_REGISTRAR
@@ -126,37 +141,47 @@ EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 3, p_idDistrito => 3, p_nombre => 
 EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 4, p_idDistrito => 4, p_nombre => 'Hospital 4', p_antiguedad => 5, p_area => 100, p_idSede => 4, p_idGerente => 4, p_idCondicion => 4);
 EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 5, p_idDistrito => 5, p_nombre => 'Hospital 5', p_antiguedad => 5, p_area => 100, p_idSede => 5, p_idGerente => 5, p_idCondicion => 5);
 
-EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 6, p_idDistrito => 5, p_nombre => 'Hospital 5', p_antiguedad => 5, p_area => 100, p_idSede => 5, p_idGerente => 5, p_idCondicion => 5);
-EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 7, p_idDistrito => 5, p_nombre => 'Hospital 5', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
-EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 8, p_idDistrito => 5, p_nombre => 'Hospital 5', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 6, p_idDistrito => 5, p_nombre => 'Hospital 6', p_antiguedad => 5, p_area => 100, p_idSede => 5, p_idGerente => 5, p_idCondicion => 5);
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 7, p_idDistrito => 5, p_nombre => 'Hospital 7', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 8, p_idDistrito => 5, p_nombre => 'Hospital 8', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
+
+
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 9, p_idDistrito => 5, p_nombre => 'Hospital 9', p_antiguedad => 5, p_area => 100, p_idSede => 5, p_idGerente => 5, p_idCondicion => 5);
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 10, p_idDistrito => 5, p_nombre => 'Hospital 10', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
+EXECUTE SP_HOSPITAL_REGISTRAR(p_idHospital => 32323, p_idDistrito => 5, p_nombre => 'Hospital 11', p_antiguedad => 5, p_area => 100, p_idSede => 2, p_idGerente => 5, p_idCondicion => 5);
 -- verifica el hospital registrado
 SELECT * FROM Hospital;
 
-select*from hospital;
 
 
 
 /*
   Nombre del procedimiento: SP_HOSPITAL_ACTUALIZAR
-  Descripcion: Este procedimiento actualiza la informacion de un hospital en la tabla Hospital basada en el ID del hospital proporcionado.
+  Descripción: Este procedimiento actualiza la información de un hospital en la tabla Hospital basada en el ID del hospital proporcionado.
   Autor: Alexander
   Fecha de creación: 02-07-2024
   Versión: 1
 */
 
 
--- SP_HOSPITAL_ACTUALIZAR
 CREATE OR REPLACE PROCEDURE SP_HOSPITAL_ACTUALIZAR (
-    p_idHospital IN NUMBER,
-    p_idDistrito IN NUMBER,
+    p_idHospital IN VARCHAR2,
+    p_idDistrito IN VARCHAR2,
     p_nombre IN VARCHAR2,
-    p_antiguedad IN NUMBER,
-    p_area IN NUMBER,
-    p_idSede IN NUMBER,
-    p_idGerente IN NUMBER,
-    p_idCondicion IN NUMBER
+    p_antiguedad IN VARCHAR2,
+    p_area IN VARCHAR2,
+    p_idSede IN VARCHAR2,
+    p_idGerente IN VARCHAR2,
+    p_idCondicion IN VARCHAR2
 ) AS
+    v_idHospital NUMBER;
 BEGIN
+    -- verifica si el idHospital especificado existe
+    SELECT idHospital INTO v_idHospital
+    FROM Hospital
+    WHERE idHospital = TO_NUMBER(p_idHospital);
+
+    -- actualiza los valores en la tabla Hospital
     UPDATE Hospital
     SET idDistrito = p_idDistrito,
         nombre = p_nombre,
@@ -167,20 +192,36 @@ BEGIN
         idCondicion = p_idCondicion,
         fechaRegistro = SYSDATE
     WHERE idHospital = p_idHospital;
+
+    -- muestra el mensaje de actualización exitosa
+    DBMS_OUTPUT.PUT_LINE('Actualización exitosa.');
     COMMIT;
+EXCEPTION
+    WHEN VALUE_ERROR THEN
+        -- captura la excepcion de valor
+        DBMS_OUTPUT.PUT_LINE('Error: Uno o más campos no tienen el tipo de datos correcto.');
+        ROLLBACK;
+    WHEN NO_DATA_FOUND THEN
+        -- captura si el idHospital especificado no existe
+        DBMS_OUTPUT.PUT_LINE('Error: El idHospital especificado no existe.');
+        ROLLBACK;
+    WHEN OTHERS THEN
+        -- captura cualquier otra excepcion
+        DBMS_OUTPUT.PUT_LINE('Error al actualizar el hospital: ' || SQLERRM);
+        ROLLBACK;
 END SP_HOSPITAL_ACTUALIZAR;
 
 
 -- ejecucion del procedimiento almacenado SP_HOSPITAL_ACTUALIZAR
-EXECUTE SP_HOSPITAL_ACTUALIZAR(p_idHospital => 1, p_idDistrito => 2, p_nombre => 'Hostipal actualizado', p_antiguedad => 6, p_area => 200, p_idSede => 2, p_idGerente => 2, p_idCondicion => 2);
+EXECUTE SP_HOSPITAL_ACTUALIZAR(p_idHospital => 2, p_idDistrito => 2, p_nombre => 'Hostipal actualizado', p_antiguedad => 6, p_area => 200, p_idSede => 1, p_idGerente => 1, p_idCondicion => 2);
 -- verifica el hospital actualizado
-SELECT * FROM Hospital WHERE idHospital = 1;
+SELECT * FROM Hospital WHERE idHospital = 2;
 
 
 
 /*
   Nombre del procedimiento: SP_HOSPITAL_ELIMINAR
-  Descripcion: Este procedimiento elimina un registro de la tabla Hospital basado en el ID del hospital proporcionado.
+  Descripción: Este procedimiento elimina un registro de la tabla Hospital basado en el ID del hospital proporcionado.
   Autor: Alexander
   Fecha de creación: 02-07-2024
   Versión: 1
@@ -188,31 +229,45 @@ SELECT * FROM Hospital WHERE idHospital = 1;
 
 -- SP_HOSPITAL_ELIMINAR
 CREATE OR REPLACE PROCEDURE SP_HOSPITAL_ELIMINAR (
-    p_idHospital IN NUMBER
+    p_idHospital IN VARCHAR2
 ) AS
+    v_hospital_id NUMBER;
 BEGIN
+    -- verifica si el hospital existe
+    SELECT idHospital INTO v_hospital_id FROM Hospital WHERE idHospital = p_idHospital;
+
+    -- elimina el hospital
     DELETE FROM Hospital WHERE idHospital = p_idHospital;
     COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('El hospital con el ID ' || p_idHospital || ' ha sido eliminado correctamente');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('El hospital con el ID ' || p_idHospital || ' no existe.');
+        ROLLBACK;
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al eliminar el hospital: ' || SQLERRM);
+        ROLLBACK;
 END SP_HOSPITAL_ELIMINAR;
 
 
+
 -- ejecucion del procedimiento almacenado SP_HOSPITAL_ELIMINAR
-EXECUTE SP_HOSPITAL_ELIMINAR(p_idHospital => 1);
+EXECUTE SP_HOSPITAL_ELIMINAR(p_idHospital => 8);
 -- verifica que el hospital se elimino
-SELECT * FROM Hospital WHERE idHospital = 1;
+SELECT * FROM Hospital WHERE idHospital = 8;
 
 
 
 /*
   Nombre del procedimiento: SP_HOSPITAL_LISTAR
-  Descripcion: Este procedimiento realiza una busqueda en la tabla Hospital basada en un campo y un valor especifico
+  Descripción: Este procedimiento realiza una busqueda en la tabla Hospital basada en un campo y un valor específico
   Autor: alexander
   Fecha de creación: 02-07-2024
   Versión: 1
 */
 
 
--- SP_SP_HOSPITAL_LISTAR
 CREATE OR REPLACE PROCEDURE SP_HOSPITAL_LISTAR (
     p_field_name IN VARCHAR2,
     p_field_value IN VARCHAR2
@@ -226,11 +281,13 @@ CREATE OR REPLACE PROCEDURE SP_HOSPITAL_LISTAR (
     v_idGerente Hospital.idGerente%TYPE;
     v_idCondicion Hospital.idCondicion%TYPE;
     v_fechaRegistro Hospital.fechaRegistro%TYPE;
+    result_cursor SYS_REFCURSOR;
 BEGIN
     -- consulta dinamica
     OPEN result_cursor FOR
-        'SELECT idHospital, nombre, antiguedad, area, idSede, idGerente, idCondicion, fechaRegistro FROM Hospital WHERE ' || p_field_name || ' = :field_value'
-        USING p_field_value;
+        'SELECT idHospital, nombre, antiguedad, area, idSede, idGerente, idCondicion, fechaRegistro FROM Hospital WHERE ' || p_field_name || ' LIKE :field_value'
+        USING '%' || p_field_value || '%';
+
 
     -- muestra los registros obtenidos
     DBMS_OUTPUT.PUT_LINE('Registros encontrados para el campo ' || p_field_name || ':');
@@ -248,8 +305,15 @@ BEGIN
                              ', Fecha de Registro: ' || TO_CHAR(v_fechaRegistro, 'DD-MON-YYYY HH24:MI:SS'));
     END LOOP;
     CLOSE result_cursor;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontraron registros para el campo ' || p_field_name || ' con el valor ' || p_field_value);
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al listar los registros: ' || SQLERRM);
 END SP_HOSPITAL_LISTAR;
 
--- ejecucion del procedimiento almacenado SP_HOSPITAL_LISTAR
-  EXECUTE SP_HOSPITAL_LISTAR('area', '100');
 
+-- ejecucion del procedimiento almacenado SP_HOSPITAL_LISTAR
+  EXECUTE SP_HOSPITAL_LISTAR('nombre', 'Hospital');
+
+SELECT*FROM HOSPITAL;
